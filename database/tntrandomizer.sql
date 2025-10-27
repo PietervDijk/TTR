665226dae55db707ead9748519ecc67c74ee76c2
PietervDijk <pieter@technolableiden.nl>
2025-10-27 10:17:41 +0100

    database

---
+++ database/tntrandomizer.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 27 okt 2025 om 10:16
+-- Serverversie: 10.4.32-MariaDB
+-- PHP-versie: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tntrandomizer`
+--
+CREATE DATABASE IF NOT EXISTS `tntrandomizer` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tntrandomizer`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `instellingen`
+--
+
+CREATE TABLE `instellingen` (
+  `aantal_voorkeuren` int(11) NOT NULL,
+  `hoofdwachtwoord` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `klas`
+--
+
+CREATE TABLE `klas` (
+  `klas_id` smallint(6) NOT NULL,
+  `school_id` smallint(6) NOT NULL,
+  `klasaanduiding` varchar(100) NOT NULL,
+  `leerjaar` varchar(100) DEFAULT NULL,
+  `schooljaar` varchar(100) NOT NULL,
+  `pincode` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `klas`
+--
+
+INSERT INTO `klas` (`klas_id`, `school_id`, `klasaanduiding`, `leerjaar`, `schooljaar`, `pincode`) VALUES
+(1, 5, 'ZWSD23F', '3', '2025-2026', 1234);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `leerling`
+--
+
+CREATE TABLE `leerling` (
+  `leerling_id` smallint(6) NOT NULL,
+  `klas_id` smallint(6) NOT NULL,
+  `voornaam` varchar(100) NOT NULL,
+  `tussenvoegsel` varchar(100) DEFAULT NULL,
+  `achternaam` varchar(100) NOT NULL,
+  `voorkeur1_wereld_sector_id` smallint(6) NOT NULL,
+  `voorkeur2_wereld_sector_id` smallint(6) NOT NULL,
+  `voorkeur3_wereld_sector_id` smallint(6) NOT NULL,
+  `toegewezen_wereld_sector_id` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `school`
+--
+
+CREATE TABLE `school` (
+  `school_id` smallint(6) NOT NULL,
+  `schoolnaam` varchar(100) NOT NULL,
+  `plaats` varchar(100) NOT NULL,
+  `type_onderwijs` set('Primair Onderwijs','Voortgezet Onderwijs','MBO') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `school`
+--
+
+INSERT INTO `school` (`school_id`, `schoolnaam`, `plaats`, `type_onderwijs`) VALUES
+(1, 'Vesterhavet', 'Hoofddorp', 'Primair Onderwijs'),
+(2, 'Spaarne College', 'Haarlem', 'Voortgezet Onderwijs'),
+(3, 'De Franciscus ', 'Bennebroek', 'Primair Onderwijs'),
+(4, 'Willinkschool', 'Bennebroek', 'Primair Onderwijs'),
+(5, 'Nova College', 'Haarlem', 'MBO');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `wereld_sector`
+--
+
+CREATE TABLE `wereld_sector` (
+  `wereld_sector_id` smallint(6) NOT NULL,
+  `naam` varchar(100) NOT NULL,
+  `type` set('wereld','sector') NOT NULL,
+  `beschrijving` varchar(100) DEFAULT NULL,
+  `actief` tinyint(1) NOT NULL,
+  `max_aantal_leerlingen` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `klas`
+--
+ALTER TABLE `klas`
+  ADD PRIMARY KEY (`klas_id`),
+  ADD KEY `school_id` (`school_id`);
+
+--
+-- Indexen voor tabel `leerling`
+--
+ALTER TABLE `leerling`
+  ADD PRIMARY KEY (`leerling_id`),
+  ADD KEY `klas_id` (`klas_id`);
+
+--
+-- Indexen voor tabel `school`
+--
+ALTER TABLE `school`
+  ADD PRIMARY KEY (`school_id`);
+
+--
+-- Indexen voor tabel `wereld_sector`
+--
+ALTER TABLE `wereld_sector`
+  ADD PRIMARY KEY (`wereld_sector_id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `klas`
+--
+ALTER TABLE `klas`
+  MODIFY `klas_id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT voor een tabel `leerling`
+--
+ALTER TABLE `leerling`
+  MODIFY `leerling_id` smallint(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `school`
+--
+ALTER TABLE `school`
+  MODIFY `school_id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT voor een tabel `wereld_sector`
+--
+ALTER TABLE `wereld_sector`
+  MODIFY `wereld_sector_id` smallint(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `klas`
+--
+ALTER TABLE `klas`
+  ADD CONSTRAINT `klas_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`);
+
+--
+-- Beperkingen voor tabel `leerling`
+--
+ALTER TABLE `leerling`
+  ADD CONSTRAINT `leerling_ibfk_1` FOREIGN KEY (`klas_id`) REFERENCES `klas` (`klas_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
