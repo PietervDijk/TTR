ffd526930d67d324f084374c87047da70bd493ab
PietervDijk <pieter@technolableiden.nl>
2025-11-26 16:24:51 +0100

    scripts meegenomen in de footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,7 @@
         <?php if (isset($current_page) && $current_page === 'scholen.php'): ?>
             <script src="javascript/scholen.js"></script>
+        <?php elseif (isset($current_page) && $current_page === 'klassen.php'): ?>
+            <script src="javascript/klassen.js"></script>
         <?php endif; ?>
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa..." crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9..." crossorigin="anonymous"></script>
