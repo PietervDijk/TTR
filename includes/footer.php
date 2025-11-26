8cb5394062af2b48632cecb8fe3449ffff581dc6
PietervDijk <pieter@technolableiden.nl>
2025-11-26 14:52:50 +0100

    aanpassingen in de footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,11 @@
+        <?php if (isset($current_page) && $current_page === 'scholen.php'): ?>
+            <script src="javascript/scholen.js"></script>
+        <?php endif; ?>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa..." crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9..." crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSD..." crossorigin="anonymous"></script>
+
+
     </body>
+
 </html>
