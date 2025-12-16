83e0b99819db9d79e7530ca9524470fe8751c75e
PietervDijk <pieter@technolableiden.nl>
2025-12-16 11:56:25 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,11 +3,10 @@
         <?php elseif (isset($current_page) && $current_page === 'klassen.php'): ?>
             <script src="javascript/klassen.js"></script>
         <?php endif; ?>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa..." crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9..." crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSD..." crossorigin="anonymous"></script>
 
-
+        <!-- Bootstrap 5.3 Bundle (incl. Popper) -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+        </script>
     </body>
-
 </html>
