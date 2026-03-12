c64812ac4e8a37bb75a22ee3238fd8a80373920a
PietervDijk <pieter@technolableiden.nl>
2026-03-12 13:26:02 +0100

    Create bezoeken.php

---
+++ bezoeken.php
@@ -0,0 +1,3 @@
+<!-- Pagina voor bezoeken, waarin je bezoekcombinaties kan invoeren met datum/tijd en ook type onderwijs.
+En vervolgens in dat scherm scholen gefilterd op het aangewezen onderwijstype kunt toevoegen en klassen van die school 
+kan toevoegen via een selectie van bestaande scholen en klassen. -->
