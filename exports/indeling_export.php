c809f56b11b5ce46ef8f78fbf9416da11971721d
PietervDijk <pieter@technolableiden.nl>
2026-05-22 14:40:31 +0200

    pagina gemaakt voor logica exportfunctie voor indeling
    pagina gemaakt voor logica exportfunctie voor indeling met commentaar-beschrijving wat nodig is.

---
+++ exports/indeling_export.php
@@ -0,0 +1,9 @@
+<?php 
+/* * Indeling exporteren
+    * 
+    * Deze pagina genereert een CSV-bestand met de indeling van de bezoeken, inclusief de bijbehorende scholen en klassen.
+    * De CSV bevat de volgende kolommen: bezoek_id, school_id, schoolnaam, plaats, klas_id, klasnaam, schooljaar.
+    * Alleen gebruikers met een geldige admin sessie kunnen deze pagina openen.
+    * De gegenereerde CSV wordt direct gedownload door de browser.
+    */
+?>
